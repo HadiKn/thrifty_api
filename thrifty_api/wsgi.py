05b343c94a5ee5bd502c9098dbfd67d247ca704b
HadiKn <hadi.padihasi@gmail.com>
2026-03-13 21:56:45 +0300

    wsgi

--- thrifty_api/wsgi.py
+++ thrifty_api/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for thrifty_api project.
+WSGI config for backend project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -7,6 +7,8 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
+from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
 import os
 
 from django.core.wsgi import get_wsgi_application
@@ -14,3 +16,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'thrifty_api.settings')
 
 application = get_wsgi_application()
+
+application = WhiteNoise(application, root=os.path.join(os.path.dirname(os.path.abspath(__file__)), 'staticfiles'))
