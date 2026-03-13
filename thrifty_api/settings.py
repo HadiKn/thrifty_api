7f529bd5004226029c34de236cd9d58b34057791
HadiKn <hadi.padihasi@gmail.com>
2026-03-13 21:49:38 +0300

    static files

--- thrifty_api/settings.py
+++ thrifty_api/settings.py
@@ -174,8 +174,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+] if os.path.exists(os.path.join(BASE_DIR, 'static')) else []
 
+STATICFILES_DIRS += [
+    os.path.join(os.path.dirname(rest_framework.__file__), 'static'),
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
