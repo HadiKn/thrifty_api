eafcfeae1f890ae0a0e143ee9960a2befe5f8de6
HadiKn <hadi.padihasi@gmail.com>
2026-08-12 03:59:01 +0300

    added claims to admin dashboard

--- items/admin.py
+++ items/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Item,Auction
+from .models import Item,Auction,Claim
 
 admin.site.register(Item)
 admin.site.register(Auction)
+admin.site.register(Claim)
